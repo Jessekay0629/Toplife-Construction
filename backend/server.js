cc121aeb7fb4fdc62427d693e1d78e4df265ab09
Jessekay0629 <168183588+Jessekay0629@users.noreply.github.com>
2025-03-21 15:17:40 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,23 +12,6 @@ app.use(cors());
 app.use(express.json());  // To parse incoming JSON requests
 app.use(express.urlencoded({ extended: true }));  // To parse incoming URL-encoded data (form submissions)
 
-// Import routes from routes.js
-
-const routes = require('./routes/routes'); // Universal/General Routes
-app.use('/', routes); 
-
-const servicesRoutes = require('./routes/services'); // Services Routes
-app.use('/services', servicesRoutes);
-
-const projectRoutes = require('./routes/projects'); // Projects/Portfolio Routes
-app.use('/api/projects', projectRoutes);
-
-app.use('/projects', projectRoutes);
-
-const contactRoutes = require('./routes/contact'); // Contact Routes
-app.use('/contact', contactRoutes);
-
-
 // MongoDB connection setup
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri)
